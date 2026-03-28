ba27152a0490f037b6eda44e5022210780590e96
caio86 <caioluiz86@gmail.com>
2026-03-28 15:36:31 -0300

    feat: create basic cli with click, and read toml config with tomli

--- src/inetrm/cli.py
+++ src/inetrm/cli.py
@@ -1,5 +1,25 @@
-def main():
-    print("Rodando")
+from inetrm import core
+import click
+
+@click.group(invoke_without_command=True)
+@click.option(
+    "--config",
+    default="config.toml",
+    help="Path to the TOML configuration file."
+)
+@click.pass_context
+def main(ctx, config):
+
+    ctx.ensure_object(dict)
+
+    cfg = core.load_config(config)
+
+    mlmodel = cfg["ml"]["model"]
+    mlfeatures = cfg["ml"]["features"]
+
+    click.echo("Rodando...")
+    click.echo(f"Model: {mlmodel}")
+    click.echo(f"Features: {mlfeatures}")
 
 if __name__ == "__main__":
     main()
