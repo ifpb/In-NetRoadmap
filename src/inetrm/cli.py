dddd23a7cfc382c1b58970841ec22a807037d22b
caio86 <caioluiz86@gmail.com>
2026-03-29 11:55:52 -0300

    feat(module_a): ensure output dir is created and removed not used code blocks from jupyter template

--- src/inetrm/cli.py
+++ src/inetrm/cli.py
@@ -5,12 +5,12 @@ import click
 from inetrm.module_a import a_logic as a
 
 
-@click.group(invoke_without_command=True)
+@click.group()
 def main():
-    click.echo("Rodando...")
+    pass
 
 
-@click.command()
+@main.command()
 @click.option(
     "--config",
     default="config.toml",
@@ -30,17 +30,17 @@ def train(ctx, config, output_dir, data):
 
     cfg = a.load_config(config)
 
-    Path(output_dir).mkdir
+    Path(output_dir).mkdir(parents=True, exist_ok=True)
 
     a.validate_model(cfg)
     a.validate_data(cfg, data)
+
+    click.echo("Generating notebook...")
     notebook_path = a.create_notebook(cfg, data, output_dir)
-    click.echo(notebook_path)
-    # a.create_symlink(config, data)
+    click.echo(click.style(f"Notebook created at: {notebook_path}", fg="green"))
+
     a.initiate_jupyter(notebook_path)
 
-
-main.add_command(train)
 
 if __name__ == "__main__":
     main()
