5ff4def5f2f6ab82d50b00259f42b26f8a4b291a
caio86 <caioluiz86@gmail.com>
2026-03-29 11:22:07 -0300

    feat: Create jupyter notebook instead of executing template
    - Add create_notebook function for creating a copy notebook from template with added variable declarations
    - Removed config.toml symlink
    - Removed unnecessary code
    blocks from jupyter notebook
    - Removed variables declarations from jupyter notebook that will be provided by config.toml
    - Updated cli.py to match a_logic.py changes

--- src/inetrm/cli.py
+++ src/inetrm/cli.py
@@ -1,29 +1,43 @@
-from inetrm.module_a import a_logic as a
+from pathlib import Path
+
 import click
-import os
+
+from inetrm.module_a import a_logic as a
+
 
 @click.group(invoke_without_command=True)
 def main():
     click.echo("Rodando...")
 
+
 @click.command()
-@click.option("--config", default="config.toml", help="Path to the TOML configuration file.")
-@click.argument('data')
+@click.option(
+    "--config",
+    default="config.toml",
+    type=click.Path(exists=True),
+    help="Path to the TOML configuration file.",
+)
+@click.option(
+    "--output-dir",
+    default=str(Path.cwd()),
+    type=click.Path(),
+    help="Path to the output dir for artifacts.",
+)
+@click.argument("data", type=click.Path(exists=True))
 @click.pass_context
-def train(ctx, config, data):
+def train(ctx, config, output_dir, data):
     ctx.ensure_object(dict)
 
     cfg = a.load_config(config)
+
+    Path(output_dir).mkdir
+
     a.validate_model(cfg)
     a.validate_data(cfg, data)
-    a.create_symlink(config, data)
-    a.initiate_jupyter(cfg)
-
-    mlmodel = cfg["ml"]["model"]
-    mlfeatures = cfg["ml"]["features"]
-
-    click.echo(f"Model: {mlmodel}")
-    click.echo(f"Features: {mlfeatures}")
+    notebook_path = a.create_notebook(cfg, data, output_dir)
+    click.echo(notebook_path)
+    # a.create_symlink(config, data)
+    a.initiate_jupyter(notebook_path)
 
 
 main.add_command(train)
--- src/inetrm/module_a/a_logic.py
+++ src/inetrm/module_a/a_logic.py
@@ -1,11 +1,12 @@
-
+import nbformat
+from nbformat.v4 import new_code_cell, new_markdown_cell
 import tomli
 import sys
 import csv
-import os
 import subprocess
 from pathlib import Path
 from inetrm.module_a.catalog import Model
+
 
 def load_config(config_path: str) -> dict:
     path = Path(config_path)
@@ -41,7 +42,44 @@ def create_symlink(config, data):
     os.symlink(f"{os.getcwd()}/{data}", f"{target}/data.csv")
     os.symlink(f"{os.getcwd()}/{config}", f"{target}/config.toml")
 
-def initiate_jupyter(config):
-    ipynb_file = Model.JUPYTER_MAPPING[config['ml']['model']]
-    target = f"{os.path.dirname(os.path.abspath(__file__))}/jupyter"
-    subprocess.run(["jupyter", "notebook", ipynb_file], cwd=target)
+def create_notebook(cfg: dict, data: str, output_dir: str) -> str:
+    model_name = cfg["ml"]["model"]
+    ipynb_file = Model.JUPYTER_MAPPING[model_name]
+
+    templates_dir = Path(__file__).resolve().parent / "jupyter"
+    template_path = templates_dir / ipynb_file
+
+    if not template_path.is_file():
+        raise ValueError(f"Invalid template path: {template_path}")
+
+    with open(template_path, "r", encoding="utf-8") as f:
+        nb = nbformat.read(f, as_version=4)
+
+    data_path = Path(data).resolve()
+    out_dir = Path(output_dir).resolve()
+    notebook_path = f"{out_dir}/{model_name}.ipynb"
+    features_list = cfg["ml"]["features"]
+
+    vars_code = "\n".join(
+        [
+            f"input_dataset = {repr(str(data_path))}",
+            f'output_dir = "{out_dir}"',
+            f'output_tree = "{out_dir}/tree.txt"',
+            f"features = {repr(features_list)}",
+        ]
+    )
+
+    md_cell = new_markdown_cell("# Variáveis")
+    code_cell = new_code_cell(vars_code)
+
+    nb.cells.insert(1, md_cell)
+    nb.cells.insert(2, code_cell)
+
+    with open(notebook_path, "w", encoding="utf-8") as f:
+        nbformat.write(nb, f)
+
+    return notebook_path
+
+
+def initiate_jupyter(notebook_path):
+    subprocess.run(["jupyter", "notebook", notebook_path])
