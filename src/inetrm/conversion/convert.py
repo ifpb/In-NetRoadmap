1f3c7de436429ba7833683240f4823068696380b
icaruuuuuu <icaromachadosilva@gmail.com>
2026-04-12 21:29:27 -0300

    match not supported in python 3.9

--- src/inetrm/conversion/convert.py
+++ src/inetrm/conversion/convert.py
@@ -5,13 +5,11 @@ from .random_forest import convert_random_forest
 def convert(cfg: dict, model, p4_output_path: str, table_output_path: str):
     model_type = cfg.get("ml", {}).get("model")
 
-    match model_type:
-        case "decision_tree":
-            convert_decision_tree(cfg, model, p4_output_path, table_output_path)
-        case "random_forest":
-            convert_random_forest(cfg, model, p4_output_path, table_output_path)
-
-        case _:
-            raise ValueError(
-                f"Unsupported model type defined in configuration: '{model_type}'"
-            )
+    if model_type == "decision-tree":
+        convert_decision_tree(cfg, model, p4_output_path, table_output_path)
+    elif model_type == "random_forest":
+        convert_random_forest(cfg, model, p4_output_path, table_output_path)
+    else:
+        raise ValueError(
+            f"Unsupported model type defined in configuration: '{model_type}'"
+        )
