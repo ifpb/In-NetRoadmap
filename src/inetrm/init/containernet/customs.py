a205cfd2709057906433ae1a60a50919b8666dfa
icaruuuuuu <icaromachadosilva@gmail.com>
2026-08-04 00:32:20 -0300

    modularization of topology.py for renegeration purposes

---
+++ src/inetrm/init/containernet/customs.py
@@ -0,0 +1,8 @@
+def orchestrate(nodes_dict, duration):
+    """ 
+    Insert custom orchestration here.
+    Nodes are called via nodes_dict['node_name']. Use the .cmd method to call bash commands.
+    e.g.: nodes_dict['h1'].cmd(f"bash -c 'scripts/custom-script.sh {duration}'")
+    """
+
+    return
