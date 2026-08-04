598b58a6055a5a1949ac6b1d91d7b68da9dec22f
icaruuuuuu <icaromachadosilva@gmail.com>
2026-08-03 21:00:20 -0300

    added naive bayes conversion module

--- src/inetrm/conversion/naive_bayes/naive_bayes.py
+++ src/inetrm/conversion/naive_bayes/naive_bayes.py
@@ -1,12 +1,16 @@
 from .generate_p4 import generate_p4
-from .generate_table import generate_table
+from .generate_tables import generate_tables
 from .read_model import extract_model
 
 def convert_naive_bayes(
     cfg: dict, model, p4_output_path: str, table_output_path: str
 ) -> None:
+    """
+    Função principal que orquestra a conversão de um modelo Gaussian Naive Bayes
+    treinado em código P4 e tabelas de roteamento correspondentes.
+    """
     features_list = cfg["ml"]["features"]
     res = extract_model(model, features_list)
-
-    generate_p4(res["n_classes"], features_list, p4_output_path)
-    generate_table(res, table_output_path)
+    n_classes = len(res["classes"])
+    generate_p4(n_classes, features_list, p4_output_path)
+    generate_tables(res, table_output_path)
